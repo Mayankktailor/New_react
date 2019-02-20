6d3c8245ad7ddec575cbcbb70293b2b56b952127
Mayankt128 <mayanktailork@gmail.com>
2019-02-20 11:23:02 +0530

    First commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { getOrder } from '../actions/formAction';
 import {connect} from 'react-redux';
 import { Button } from 'reactstrap';
